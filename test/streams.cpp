af7e2fc974f7a2ddda85f9e5c92397427f5bd71d
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2015-10-01 22:19:28 +0200

    Add simple streams unit test

--- test/streams.cpp
+++ test/streams.cpp
@@ -34,24 +34,71 @@ TEST_CASE("simple stream construction and manipulation", "[streams]") {
 
     stream.feedElements(matrix);
 
+    std::array<TIdx, 3> procs = { 0, 1, 4 };
+
+    std::array<std::array<float, 4>, 3> firstBlock = {
+        0.0f, 1.0f, 16.0f, 17.0f,
+        2.0f, 3.0f, 18.0f, 19.0f,
+        32.0f, 33.0f, 48.0f, 49.0f
+    };
+
+    std::array<std::array<float, 4>, 3> secondBlock = {
+        8.0f, 9.0f, 24.0f, 25.0f,
+        10.0f, 11.0f, 26.0f, 27.0f,
+        40.0f, 41.0f, 56.0f, 57.0f
+    };
+
+    std::array<std::array<float, 4>, 3> thirdBlock = {
+        128.0f, 129.0f, 144.0f, 145.0f,
+        130.0f, 131.0f, 146.0f, 147.0f,
+        160.0f, 161.0f, 176.0f, 177.0f
+    };
+
     SECTION("the stream is LHS by default") {
         auto& data = stream.getData();
+
+        for (unsigned int i = 0; i < 3; ++i) {
+            CHECK(std::equal(firstBlock[i].begin(), firstBlock[i].end(),
+                               data[procs[i]].begin()));
+        }
+
+        for (unsigned int i = 0; i < 3; ++i) {
+            CAPTURE(i);
+            CHECK(std::equal(secondBlock[i].begin(), secondBlock[i].end(),
+                               data[procs[i]].begin() + l * l));
+        }
+
+        for (unsigned int i = 0; i < 3; ++i) {
+            CAPTURE(i);
+            CHECK(std::equal(thirdBlock[i].begin(), thirdBlock[i].end(),
+                               data[procs[i]].begin() + 2 * l * l));
+        }
     }
 
     SECTION("we can transpose the stream") {
         stream.setOrientation(stream_orientation::right_handed);
         auto& data = stream.getData();
-        ZeeLogVar(data[0]);
-        ZeeLogVar(data[1]);
-        ZeeLogVar(data[2]);
-        ZeeLogVar(data[3]);
-        ZeeLogVar(data[5]);
-        ZeeLogVar(data[8]);
-        ZeeLogVar(data[15]);
+
+        for (unsigned int i = 0; i < 3; ++i) {
+            CHECK(std::equal(firstBlock[i].begin(), firstBlock[i].end(),
+                               data[procs[i]].begin()));
+        }
+
+        for (unsigned int i = 0; i < 3; ++i) {
+            CAPTURE(i);
+            CHECK(std::equal(thirdBlock[i].begin(), thirdBlock[i].end(),
+                               data[procs[i]].begin() + l * l));
+        }
+
+        for (unsigned int i = 0; i < 3; ++i) {
+            CAPTURE(i);
+            CHECK(std::equal(secondBlock[i].begin(), secondBlock[i].end(),
+                               data[procs[i]].begin() + 2 * l * l));
+        }
     }
 }
 
-/* TEST_CASE("stream construction and manipulation using matrices", "[streams]") {
+TEST_CASE("stream construction and manipulation using matrices", "[streams]") {
     const int n = 128;
 
     DStreamingMatrix<TVal, TIdx> A(n, n);
@@ -135,4 +182,4 @@ TEST_CASE("we can multiply two streamed matrices", "[streams]") {
     // C = A * B;
 
 
-} */
+}
